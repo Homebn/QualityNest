05b88bfd1b6714079136864ae66296911a97f25a
Homebn <Andrkot0304@gmail.com>
2022-02-01 14:17:17 +0300

    Commit

---
+++ sandbox/src/main/java/classes/Point.java
@@ -0,0 +1,17 @@
+package classes;
+
+
+public class Point {
+
+   public double x;
+   public double y;
+
+    public Point(int x,int y)
+    {
+        this.x=x;
+        this.y=y;
+
+    }
+
+    }
+
--- sandbox/src/main/java/main/java/Main.java
+++ sandbox/src/main/java/main/java/Main.java
@@ -1,18 +1,23 @@
 package main.java;
 
 
-import java.util.Scanner;
+
+import classes.Point;
+
 
 public class Main {
+    public static double Dest(Point a, Point b) {
+        //Расстояние между точками
+        double sum2 = (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y);
+        double des = Math.sqrt(sum2);
+        return des;
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите число:");
-
-        int number = sc.nextInt();
-
-        System.out.println("Спасибо! Вы ввели число " + number);
+        Point p = new Point(1, 1);
+        Point p1 = new Point(1,1);
+        System.out.println(Dest(p, p1));
     }
-    }
+}
 
 
