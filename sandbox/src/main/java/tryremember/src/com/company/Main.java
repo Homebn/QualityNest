f6a1ee37791488942278e490816d611e418f305c
Homebn <Andrkot0304@gmail.com>
2022-02-01 13:24:39 +0300

    Commit

---
+++ sandbox/src/main/java/tryremember/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package tryremember.src.com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число:");
+
+        int number = sc.nextInt();
+
+        System.out.println("Спасибо! Вы ввели число " + number);
+    }
+    }
+
+
